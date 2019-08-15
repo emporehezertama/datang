1633407b6f5b3e67dade99d6b961c6887b0da0c2
Doni <doni.enginer@gmail.com>
2019-08-15 11:02:57 +0700

    Update - MHR mobile attendance

--- app/Models/UsersMhr.php
+++ app/Models/UsersMhr.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersMhr extends Model
 {
-    //
    	protected $connection = 'mysqlMhr';
+
     protected $table = 'users';
+
+    /**
+     * Absensi Setting
+     * @return void
+     */
+    public function absensiSetting()
+    {
+    	return $this->hasOne('App\Models\AbsensiSettingMhr', 'id', 'absensi_setting_id');
+    }
 }
