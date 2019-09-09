e0dce05024b7e1744f79ff884d8976f58486df41
Doni <doni.enginer@gmail.com>
2019-09-09 19:01:47 +0700

    Update structure organisasi-rev7

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -86,6 +86,7 @@ class AuthController extends Controller {
         $params['company']  = get_setting('attendance_company', $user);
         $params['news']     = get_setting('attendance_news', $user);
         $params['job']      = '';
+        $params['date']      = date('d F Y');
 
         if(isset($user->structure->position->name))
         {
@@ -154,6 +155,7 @@ class AuthController extends Controller {
         $params['company']  = get_setting('attendance_company', $user);
         $params['news']     = get_setting('attendance_news', $user);
         $params['job']      = '';
+        $params['date']      = date('d F Y');
 
         if(isset($user->structure->position->name))
         {
@@ -221,6 +223,7 @@ class AuthController extends Controller {
         $params['company']  = get_setting('attendance_company', $user);
         $params['news']     = get_setting('attendance_news', $user);
         $params['job']      = '';
+        $params['date']      = date('d F Y');
 
         if(isset($user->structure->position->name))
         {
